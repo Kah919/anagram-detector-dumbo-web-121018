72053e16471bde455d1e178cd5443d6be3634f7a
github username <github email address>
2018-12-18 15:55:46 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,34 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    array_count = Hash.new(0)
+    word_count = Hash.new(0)
+    anagrams = []
+
+    word.each_char do |letter|
+      word_count[letter] += 1
+    end
+
+    array.each do |words|
+      if words.length == word.length
+        words.each_char do |letters|
+          array_count[letters] += 1
+        end
+
+        if word_count == array_count
+          anagrams << words
+        end
+        array_count = Hash.new(0)
+      end
+    end
+    return anagrams
+  end
+end
